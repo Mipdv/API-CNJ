5673d60986709fe6da6a99df3b80458521c0cac7
Henrique Marinho <marinhodireito1@gmail.com>
2026-08-10 17:15:25 -0300

    Acrescendtado documentacao via swagger e Cors

--- src/main/java/com/Mipdv/api_consulta_cnj/business/controller/ProcessoController.java
+++ src/main/java/com/Mipdv/api_consulta_cnj/business/controller/ProcessoController.java
@@ -2,6 +2,9 @@ package com.Mipdv.api_consulta_cnj.business.controller;
 
 import com.Mipdv.api_consulta_cnj.business.dtoResponse.ProcessoDTOResponse;
 import com.Mipdv.api_consulta_cnj.business.service.ProcessoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,14 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("processo")
 @RequiredArgsConstructor
+@Tag(name= "Processo_consulta", description = "Consulta processual por meio da PDPJ")
 public class ProcessoController {
 
     private final ProcessoService processoService;
 
     @PostMapping("/{numero}")
+    @Operation(summary = "Consulta processual", description = "Consulta por meio de requisição na API da PDPJ")
+    @ApiResponse(responseCode = "200", description = "Consulta realizada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Processo não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<ProcessoDTOResponse> consultarProcesso(
             @PathVariable String numero) {
         return ResponseEntity.ok(processoService.consultarProcesso(numero));
-        //
     }
 }
